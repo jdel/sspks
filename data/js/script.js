b2848b4c94a7f3a1a6f818e3771ca14e5ffbadee
Markus Birth <mbirth@gmail.com>
2016-12-31 00:36:45 +0100

    Fix missing "var" in JavaScript.

--- data/js/script.js
+++ data/js/script.js
@@ -2,7 +2,7 @@ function toggleClass(class_name, element_ids)
 {
     for (var i in element_ids) {
         if (!element_ids.hasOwnProperty(i)) continue;
-        element = document.getElementById(element_ids[i]);
+        var element = document.getElementById(element_ids[i]);
         if (element.className.indexOf(class_name) != -1) {
             // class already there, remove it
             element.className = element.className.replace(class_name, '').trim();

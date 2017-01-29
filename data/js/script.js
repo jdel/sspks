b33649b8fd67e6c7af46c967c9555758d4161a87
Markus Birth <mbirth@gmail.com>
2017-01-29 18:53:47 +0100

    CSS transition for showing/hiding package details.

--- data/js/script.js
+++ data/js/script.js
@@ -1,15 +1,20 @@
+function toggleClassElement(class_name, element)
+{
+    if (element.className.indexOf(class_name) != -1) {
+        // class already there, remove it
+        element.className = element.className.replace(class_name, '').trim();
+    } else {
+        // class not there, add it
+        element.className += ' ' + class_name;
+    }
+}
+
 function toggleClass(class_name, element_ids)
 {
     for (var i in element_ids) {
         if (!element_ids.hasOwnProperty(i)) continue;
         var element = document.getElementById(element_ids[i]);
-        if (element.className.indexOf(class_name) != -1) {
-            // class already there, remove it
-            element.className = element.className.replace(class_name, '').trim();
-        } else {
-            // class not there, add it
-            element.className += ' ' + class_name;
-        }
+        toggleClassElement(class_name, element);
     }
 }
 
@@ -31,23 +36,12 @@ function toggleDetails(clicked_element)
 {
     var card = clicked_element.parentElement.parentElement;
     var details = card.getElementsByClassName('spk-details')[0];
-    console.log('Element: %o', details);
-    if (details.style.display) {
-        // Make details visible
-        details.style.display = "";
+    toggleClassElement('spk-details-hidden', details);
+    if (details.className.indexOf('spk-details-hidden') == -1) {
+        // Details now visible
         clicked_element.innerHTML = 'Hide Info';
     } else {
-        // Hide details
-        details.style.display = "none";
+        // Details now hidden
         clicked_element.innerHTML = 'More Info';
-    }
-}
-
-function hideAllDetails()
-{
-    var elements = document.getElementsByClassName('spk-details');
-    for (var i in elements) {
-        if (!elements.hasOwnProperty(i)) continue;
-        elements[i].style.display = "none";
     }
 }

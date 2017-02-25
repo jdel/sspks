62d1249de5c9bcef91d2b8ede175cabf54dcd831
benrubson <ben.rubson@gmail.com>
2017-02-25 17:27:15 +0100

    Make Quick Inst enabled by default, fix test

--- tests/JsonOutputTest.php
+++ tests/JsonOutputTest.php
@@ -64,7 +64,7 @@ class JsonOutputTest extends TestCase
             'gives you the ability to run thousands of containers created by developers from all over the world on DSM. The hugely popular built-in image repository, Docker ' .
             'Hub, allows you to find shared applications from other talented developers.","price":0,"download_count":6000,"recent_download_count":1222,"link":"http://prefix' . $this->tempPkg .
             '","size":' . $pkgSize . ',"md5":"' . $pkgMd5 . '","thumbnail":["http://prefix' . $p->thumbnail[0] . '","http://prefix' . $p->thumbnail[1] . '"],' .
-            '"snapshot":["http://prefix' . $p->snapshot[0] . '","http://prefix' . $p->snapshot[1] . '"],"qinst":false,"qstart":false,"qupgrade":false,"depsers":null,"deppkgs"' .
+            '"snapshot":["http://prefix' . $p->snapshot[0] . '","http://prefix' . $p->snapshot[1] . '"],"qinst":true,"qstart":true,"qupgrade":true,"depsers":null,"deppkgs"' .
             ':null,"conflictpkgs":null,"start":true,"maintainer":"Synology Inc.","maintainer_url":"http://dummy.org/","distributor":"SSpkS","distributor_url":"http://dummy.org/",' .
             '"changelog":"","thirdparty":true,"category":0,"subcategory":0,"type":0,"silent_install":true,"silent_uninstall":true,"silent_upgrade":true,"beta":false}],"keyrings":["test\n12345"]}'
         );

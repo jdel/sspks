c79c808816110faebe5c0cbd76d854e4c71d92b1
Julien Del-Piccolo <moi@jdel.org>
2017-03-11 04:02:39 +0100

    Fix

--- lib/SSpkS/Output/HtmlOutput.php
+++ lib/SSpkS/Output/HtmlOutput.php
@@ -31,8 +31,8 @@ class HtmlOutput
         $this->setVariable('baseUrlRelative', $this->config->baseUrlRelative);
         $this->setVariable('themeUrl', $this->config->baseUrlRelative . $this->config->paths['themes'] . $this->config->site['theme'] . '/');
         $this->setVariable('requestUri', $_SERVER['REQUEST_URI']);
-        if (array_key_exists('SSPKS_COMMIT', $_ENV)){
-            $this->setVariable('commitHash', $_ENV['SSPKS_COMMIT']);
+        if (array_key_exists('SSPKS_COMMIT', $_SERVER)){
+            $this->setVariable('commitHash', $_SERVER['SSPKS_COMMIT']);
         }
     }
 

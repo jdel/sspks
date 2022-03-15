4745c290c34b6a992e0793ebc07ab18fb5d2d89a
Björn Strausmann <bjoern@strausmann.net>
2022-03-15 12:21:01 +0000

    Added optimize the detection of https requrest

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ use \SSpkS\Handler;
 
 $config = new Config(__DIR__, 'conf/sspks.yaml');
 $config->baseUrlRelative = substr($_SERVER['REQUEST_URI'], 0, strrpos($_SERVER['REQUEST_URI'], '/')) . '/';
-$config->baseUrl = 'http' . (array_key_exists('HTTPS', $_SERVER) && $_SERVER['HTTPS']?'s':'') . '://' . $_SERVER['HTTP_HOST'] . $config->baseUrlRelative;
+$config->baseUrl = 'http' . ((array_key_exists('HTTP_X_FORWARDED_PROTO', $_SERVER) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == "https" || (array_key_exists('HTTPS', $_SERVER) && $_SERVER['HTTPS']))?'s':'') . '://' . $_SERVER['HTTP_HOST'] . $config->baseUrlRelative;
 
 $handler = new Handler($config);
 $handler->handle();
